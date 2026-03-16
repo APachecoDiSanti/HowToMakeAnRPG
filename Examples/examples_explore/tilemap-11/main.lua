7ee54444e622d196fe69fbc4a8a41305a29a102f
Agustín Pacheco Di Santi <pachecodisanti@outlook.com>
2026-03-15 22:02:12 -0300

    UV coordinates and tile atlas to load map

--- Examples/examples_explore/tilemap-11/main.lua
+++ Examples/examples_explore/tilemap-11/main.lua
@@ -3,8 +3,46 @@ LoadLibrary("Sprite")
 LoadLibrary("System")
 LoadLibrary("Texture")
 
+
+function array_as_str(a)
+  local str = "{ "
+  for i=1, #a do
+    if type(a[i]) == "table" then
+        str = str .. array_as_str(a[i]) .. ",\n"
+    else
+        str = str .. a[i] .. ", "
+    end
+  end
+  str = str .. "}"
+  return str
+end
+
+
+function get_uvs(atlas_size, tile_size, atlas_rows, atlas_cols)
+    -- Atlas and tiles are assumed to be square so only width or height are needed for calculations
+    -- UVs go from (0,0) in top left to (1,1) in bottom right
+    -- We need to calculate the values for each tile: top left and bottom right, so 4 values for 2 coordinates
+    tile_uv_size = tile_size / atlas_size
+    u = 0
+    v = 0
+    tile_uvs = {}
+    tile_n = 1
+    for row = 1, atlas_rows do
+        for col = 1, atlas_cols do
+            tile_uvs[tile_n] = {u, v, tile_uv_size * col, tile_uv_size * row}
+            u = u + tile_uv_size
+            tile_n = tile_n + 1
+        end
+        v = v + tile_uv_size
+    end
+    return tile_uvs
+end
+
+
 gTextureAtlas = Texture.Find("atlas.png")
 
+
+-- Our atlas has the tiles organized in the top row from 1 to 11
 gMap =
 {
     1,1,1,1,5,6, 7,1,   -- 1
@@ -21,6 +59,9 @@ gMapHeight = 7
 
 gTileWidth = 32
 gTileHeight = 32
+
+gMapUvs = get_uvs(gTextureAtlas:GetWidth(), gTileWidth, 1, 11)
+print(array_as_str(gMapUvs))
 
 gDisplayWidth = System.ScreenWidth()
 gDisplayHeight = System.ScreenHeight()
@@ -44,7 +85,9 @@ function update()
     for j = 0, gMapHeight - 1 do
         for i = 0, gMapWidth - 1 do
             local tile = GetTile(gMap, gMapWidth, i, j)
+            gTileSprite:SetUVs(unpack(gMapUvs[tile]))
             gTileSprite:SetPosition(gLeft + i * gTileWidth, gTop - j * gTileHeight)
+            gRenderer:DrawSprite(gTileSprite)
         end
     end
 end
