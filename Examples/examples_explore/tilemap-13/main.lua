58ea3189617dc1621f04a5fb69567352e7f2f6e3
Agustín Pacheco Di Santi <pachecodisanti@outlook.com>
2026-03-17 01:21:52 -0300

    Rendered Lua map exported from Tiled

--- Examples/examples_explore/tilemap-13/main.lua
+++ Examples/examples_explore/tilemap-13/main.lua
@@ -2,12 +2,59 @@ LoadLibrary("Renderer")
 LoadLibrary("Sprite")
 LoadLibrary("System")
 LoadLibrary("Texture")
+LoadLibrary("Asset")
 
-gTextureAtlas = Texture.Find("atlas.png")
+Asset.Run("example_map.lua")
 
+function array_as_str(a)
+  local str = "{ "
+  for i=1, #a do
+    if type(a[i]) == "table" then
+        str = str .. array_as_str(a[i]) .. ",\n"
+    else
+        str = str .. a[i] .. ", "
+    end
+  end
+  str = str .. "}"
+  return str
+end
 
-gTileWidth = 32
-gTileHeight = 32
+function GenerateUVs(tileWidth, tileHeight, texture)
+    -- Atlas and tiles are assumed to be square so only width or height are needed for calculations
+    -- UVs go from (0,0) in top left to (1,1) in bottom right
+    -- We need to calculate the values for each tile: top left and bottom right, so 4 values for 2 coordinates
+    local uvs = {}
+    local textureWidth = texture:GetWidth()
+    local textureHeight = texture:GetHeight()
+    local width = tileWidth / textureWidth
+    local height = tileHeight / textureHeight
+    local cols = textureWidth / tileWidth
+    local rows = textureHeight / tileHeight
+
+    -- Start from top left
+    local ux = 0
+    local uy = 0
+    local vx = width
+    local vy = height
+
+    for row = 0, rows - 1 do
+        for col = 0, cols - 1 do
+            table.insert(uvs, {ux + width * col, uy + height * row, vx + width * col, vy + height * row})
+        end
+    end
+    return uvs
+end
+
+-- Get data from exported map
+gTiledMap = CreateMap1()
+gTextureAtlas = Texture.Find(gTiledMap.tilesets[1].image)
+gMap = gTiledMap.layers[1]
+gMapHeight = gMap.height
+gMapWidth = gMap.width
+gTileWidth = gTiledMap.tilesets[1].tilewidth
+gTileHeight = gTiledMap.tilesets[1].tileheight
+gTiles = gMap.data
+gUVs = GenerateUVs(gTileWidth, gTileHeight, gTextureAtlas)
 
 gDisplayWidth = System.ScreenWidth()
 gDisplayHeight = System.ScreenHeight()
@@ -28,13 +75,13 @@ gTileSprite = Sprite.Create()
 gTileSprite:SetTexture(gTextureAtlas)
 
 function update()
-    -- for j = 0, gMapHeight - 1 do
-    --     for i = 0, gMapWidth - 1 do
-    --         local tile = GetTile(gMap, gMapWidth, i, j)
-    --         local uvs = gUVs[tile]
-    --         gTileSprite:SetUVs(unpack(uvs))
-    --         gTileSprite:SetPosition(gLeft + i * gTileWidth, gTop - j * gTileHeight)
-    --         gRenderer:DrawSprite(gTileSprite)
-    --     end
-    -- end
+    -- Map gets drawn every frame
+    for j = 0, gMapHeight - 1 do
+        for i = 0, gMapWidth - 1 do
+            local tile = GetTile(gTiles, gMapWidth, i, j)
+            gTileSprite:SetUVs(unpack(gUVs[tile]))
+            gTileSprite:SetPosition(gLeft + i * gTileWidth, gTop - j * gTileHeight)
+            gRenderer:DrawSprite(gTileSprite)
+        end
+    end
 end
