c81792a532ef1f97f6b33b792c3b2cb783d65bab
Agustín Pacheco Di Santi <pachecodisanti@outlook.com>
2026-04-01 19:31:07 -0300

    Implement GUI Panel with small 9x9 px texture and scaling

---
+++ Examples/examples_explore/gui-1/Panel.lua
@@ -0,0 +1,81 @@
+Panel = {}
+Panel.__index = Panel
+
+
+function Panel:Create(params)
+  local this = {
+    mTexture = params.texture,
+    mUVs = GenerateUVs(params.size, params.size, params.texture),
+    mTileSize = params.size,
+    mTiles = {}, -- Sprites representing the border
+  }
+
+  -- Create a sprite for each tile of the panel
+  for k, v in ipairs(this.mUVs) do
+    local sprite = Sprite:Create()
+    sprite:SetTexture(this.mTexture)
+    sprite:SetUVs(unpack(v))
+    this.mTiles[k] = sprite
+  end
+
+  setmetatable(this, self)
+  return this
+end
+
+
+-- The parameters form two points: Top-Left and Bottom-Right
+function Panel:Position(left, top, right, bottom)
+  -- Reset scales
+  for _, v in ipairs(self.mTiles) do
+    v:SetScale(1, 1)
+  end
+
+  local hSize = self.mTileSize / 2
+
+  -- Align the corner tiles
+  self.mTiles[1]:SetPosition(left + hSize, top - hSize)
+  self.mTiles[3]:SetPosition(right - hSize, top - hSize)
+  self.mTiles[7]:SetPosition(left + hSize, bottom + hSize)
+  self.mTiles[9]:SetPosition(right - hSize, bottom + hSize)
+
+  -- Calculate how much to scale the side tiles
+  local widthScale = (math.abs(right - left) - (2 * self.mTileSize)) / self.mTileSize
+  local centerX = (right + left) / 2
+
+  -- Top
+  self.mTiles[2]:SetPosition(centerX, top - hSize)
+  self.mTiles[2]:SetScale(widthScale, 1)
+
+  -- Bottom
+  self.mTiles[8]:SetPosition(centerX, bottom + hSize)
+  self.mTiles[8]:SetScale(widthScale, 1)
+
+  local heightScale = (math.abs(bottom - top) - (2 * self.mTileSize)) / self.mTileSize
+  local centerY = (bottom + top) / 2
+
+  -- Left
+  self.mTiles[4]:SetScale(1, heightScale)
+  self.mTiles[4]:SetPosition(left + hSize, centerY)
+
+  -- Right
+  self.mTiles[6]:SetScale(1, heightScale)
+  self.mTiles[6]:SetPosition(right - hSize, centerY)
+
+  -- Scale the middle backing panel
+  self.mTiles[5]:SetScale(widthScale, heightScale)
+  self.mTiles[5]:SetPosition(centerX, centerY)
+
+  -- Hide corner tiles when scale is equal to zero
+  if left - right == 0 or top - bottom == 0 then
+    for _, v in ipairs(self.mTiles) do
+      v:SetScale(0, 0)
+    end
+  end
+end
+
+
+function Panel:Render(renderer)
+  for _, v in ipairs(self.mTiles) do
+    renderer:DrawSprite(v)
+  end
+end
--- Examples/examples_explore/gui-1/main.lua
+++ Examples/examples_explore/gui-1/main.lua
@@ -9,6 +9,20 @@ LoadLibrary("Keyboard")
 Asset.Run("Util.lua")
 Asset.Run("Panel.lua")
 
-function update()
 
+gRenderer = Renderer.Create()
+
+local gPanel = Panel:Create {
+  texture = Texture.Find("simple_panel.png"),
+  size = 3,
+}
+local left = -100
+local top = 0
+local right = 100
+local bottom = -100
+gPanel:Position(left, top, right, bottom)
+
+
+function update()
+  gPanel:Render(gRenderer)
 end
