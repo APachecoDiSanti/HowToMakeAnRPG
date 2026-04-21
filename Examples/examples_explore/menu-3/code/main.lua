da3dd1b6b430aa7e51d0790b4c302f42b09923e3
Agustín Pacheco Di Santi <pachecodisanti@outlook.com>
2026-04-20 22:04:52 -0300

    Adding CreateBlock to stack to prevent other states from being updated

--- Examples/examples_explore/menu-3/code/main.lua
+++ Examples/examples_explore/menu-3/code/main.lua
@@ -9,45 +9,36 @@ mapDef.actions = {}
 mapDef.trigger_types = {}
 mapDef.triggers = {}
 
--- 11, 3, 1 == x, y, layer
+
+-- Prevents any states below it from being updated until it reaches the top and removes itself
+local CreateBlock = function(stack)
+    return {
+        Enter = function() end,
+        Exit = function() end,
+        HandleInput = function(self)
+            stack:Pop()
+        end,
+        Render = function() end,
+        Update = function(self)
+            return false
+        end
+    }
+end
 
 
 local stack = StateStack:Create()
 local state = ExploreState:Create(stack, mapDef, Vector.Create(11, 3, 1))
 stack:Push(state)
-stack:PushFit(gRenderer, 0,0,"You're trapped in a small room.")
-
--- stack:Push
--- {
---     OnEnter = function() end,
---     OnExit = function() end,
---     HandleInput = function() end,
---     Render = function(self, renderer)
-
---         local halfWidth = 100
---         local halfHeight = 100
---         renderer:DrawRect2d(
---             -halfWidth,
---             halfHeight,
---             halfWidth,
---             -halfHeight,
---             Vector.Create(0,0,0,1)
---         )
---         renderer:DrawText2d(0,0,"HELLO")
---         return false
---     end,
---     Update = function()
---         return false
---     end
--- }
+stack:Push(FadeState:Create(stack))
+stack:Push(CreateBlock(stack))
+stack:PushFit(gRenderer, 0,0,"Where am I?")
+stack:Push(CreateBlock(stack))
+stack:PushFit(gRenderer, -50,50,"My head hurts!")
+stack:Push(CreateBlock(stack))
+stack:PushFit(gRenderer, -100,100,"Uh...")
 
 function update()
     local dt = GetDeltaTime()
     stack:Update(dt)
     stack:Render(gRenderer)
-
-    if Keyboard.JustPressed(KEY_F) then
-        local fade = FadeState:Create(stack, {start = 1, finish = 0})
-        stack:Push(fade)
-    end
 end
