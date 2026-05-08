5370be82b1d9365aebae7f2903afb8b524fbedb4
Agustín Pacheco Di Santi <pachecodisanti@outlook.com>
2026-05-07 22:39:08 -0300

    Stat modifiers

--- Examples/examples_combat/stats-2/main.lua
+++ Examples/examples_combat/stats-2/main.lua
@@ -17,8 +17,70 @@ Stats:Create
     ["speed"] = 10,
     ["intelligence"] = 10,
 }
-print(stats:GetBase("intelligence"))     -- 10
-print(stats:GetBase("hp_now"))  -- 300
+
+
+function PrintStat(id)
+    local base = stats:GetBase(id)
+    local full = stats:Get(id)
+    local str = string.format("%s>%d:%d", id, base, full)
+    print(str)
+end
+
+
+PrintStat("intelligence")     -- 10
+PrintStat("hp_now")  -- 300
+PrintStat("strength")  -- 10
+
+magic_hat = {
+    id = 1,
+    modifier = {
+        add = {
+            ["strength"] = 5
+        }
+    }
+}
+
+stats:AddModifier(magic_hat.id, magic_hat.modifier)
+PrintStat("strength")  -- 15
+
+
+magic_sword = {
+    id = 2,
+    modifier = {
+        add = {
+            ["strength"] = 5
+        }
+    }
+}
+
+stats:AddModifier(magic_sword.id, magic_sword.modifier)
+PrintStat("strength")  -- 20
+
+
+spell_bravery = {
+    id = "bravery",
+    modifier = {
+        mult = {
+            ["strength"] = 0.1
+        }
+    }
+}
+
+stats:AddModifier(spell_bravery.id, spell_bravery.modifier)
+PrintStat("strength")  -- 22
+
+
+spell_curse = {
+    id = "curse",
+    modifier = {
+        mult = {
+            ["strength"] = -0.5
+        }
+    }
+}
+
+stats:AddModifier(spell_curse.id, spell_curse.modifier)
+PrintStat("strength")  -- 12 (0.1 - 0.5 == 0.4 mul)
 
 function update()
     gRenderer:DrawText2d(0, 0, "Let's make a stat class!")
