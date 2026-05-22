e856981f6fd48bc89e780d396cbd236b6a808312
Agustín Pacheco Di Santi <pachecodisanti@outlook.com>
2026-05-21 22:06:50 -0300

    Comparing equipment items

--- Examples/examples_combat/equipment-2/code/main.lua
+++ Examples/examples_combat/equipment-2/code/main.lua
@@ -9,6 +9,18 @@ gWorld.mParty:Add(Actor:Create(gPartyMemberDefs.hero))
 
 gStack:Push(ExploreState:Create(gStack, CreateArenaMap(), Vector.Create(30, 18, 1)))
 
+_, gHero = next(gWorld.mParty.mMembers)
+gBoneBlade = ItemDB[1]
+gWorldStaff = ItemDB[4]
+
+gWorld:AddItem(gBoneBlade.id)
+gHero:Equip("weapon", gWorld.mItems[1])
+
+local diff = gHero:PredictStats("weapon", gWorldStaff)
+print("Diffs")
+for k, v in pairs(diff) do
+    print(k, v)
+end
 
 function update()
     local dt = GetDeltaTime()
