942c1c29b6a9edee3064c12d927c18ad06b84a6b
Agustín Pacheco Di Santi <pachecodisanti@outlook.com>
2026-06-17 20:23:00 -0300

    Oddment table test

--- Examples/examples_combat/table-1/main.lua
+++ Examples/examples_combat/table-1/main.lua
@@ -4,6 +4,16 @@ Asset.Run('OddmentTable.lua')
 
 gRenderer = Renderer.Create()
 
+local ot = OddmentTable:Create{
+    {oddment = 1, item = "sword"},
+    {oddment = 1, item = "book"},
+}
+
+for i = 0, 50 do
+    print(ot:Pick())
+end
+
+
 function update()
     gRenderer:AlignText("center", "center")
     gRenderer:DrawText2d(0,0,"Testing the Oddment Table")
